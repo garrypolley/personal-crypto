6535945adf872ff76f03515689221b5d16c8e0b5
Garry Polley <garry.polley@c2fo.com>
2017-04-19 14:38:10 -0500

    Add link to source for code and add some docs

--- GenerateKey/Program.cs
+++ GenerateKey/Program.cs
@@ -7,6 +7,7 @@ namespace GenerateKey
     {
         static void Main(string[] args)
         {
+            // Stolen from: http://stackoverflow.com/a/17576032/1686511
             var hmac = new HMACSHA256();
             var key = Convert.ToBase64String(hmac.Key);
             Console.WriteLine($"Your key is: {key}");
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Personal Crypto
+
+This may at first appear to be a terrible idea, but it's great.
+
+This is a repo for me to manage crypto related stuff in an easy-ish way.
+
+* `GenerateKey` -- a C# cli that generates a secure key.
