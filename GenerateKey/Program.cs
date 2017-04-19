1fcd1ea222058b2acfe17da755afd0fed2c03cda
Garry Polley <garry.polley@c2fo.com>
2017-04-19 14:34:45 -0500

    Add initial generate key console command

---
+++ GenerateKey/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System;
+
+namespace GenerateKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hmac = new HMACSHA256();
+            var key = Convert.ToBase64String(hmac.Key);
+            Console.WriteLine($"Your key is: {key}");
+        }
+    }
+}
